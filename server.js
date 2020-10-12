008d12644927f9ee076094e58905c3fcd690efef
jixuni <jixuni@gmail.com>
2020-10-12 11:39:58 -0400

    testing

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const image = require("./controllers/image");
 
 const PORT = process.env.PORT || 3000;
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
   client: "pg",
   connection: {
